94c8cf6c8c97bbf77051c90ed84afb9372085d3d
Mikhail Karmishin <karmishinru@gmail.com>
2019-12-04 20:27:29 +0300

    deselect other tree on click

--- src/kostygin/Main.java
+++ src/kostygin/Main.java
@@ -10,13 +10,12 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
-import java.io.IOException;
 
 public class Main extends JFrame implements Runnable{
 
     private JScrollPane leftPane, rightPane;
     private JLabel label;
-
+    private JTree tree1, tree2;
     private DefaultMutableTreeNode root;
     private File fileRoot;
     private DefaultTreeModel treeModel;
@@ -26,25 +25,42 @@ public class Main extends JFrame implements Runnable{
         fileRoot = new File(System.getProperty("user.home"));
         root = new DefaultMutableTreeNode(new FileNode(fileRoot));
         treeModel = new DefaultTreeModel(root);
-
         ChildNodesAdder childNodesAdder = new ChildNodesAdder(fileRoot, root);
         new Thread(childNodesAdder).start();
+        tree1 = new JTree(treeModel);
+        tree2 = new JTree(treeModel);
 
-        leftPane = new JScrollPane(createFileManagerTree());
-        rightPane = new JScrollPane(createFileManagerTree());
+        deselectTree(tree1, tree2);
+        deselectTree(tree2, tree1);
+
+        leftPane = new JScrollPane(createFileManagerTree(tree1));
+        rightPane = new JScrollPane(createFileManagerTree(tree2));
 
         JPanel pane = new JPanel();
         pane.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
-        JSplitPane jSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-        jSplitPane.setLeftComponent(leftPane);
-        jSplitPane.setRightComponent(rightPane);
-
-        jSplitPane.setDividerLocation(500);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+        JMenu editMenu = new JMenu("Изменить");
+        menuBar.add(editMenu);
+        JMenu viewMenu = new JMenu("Вид");
+        menuBar.add(viewMenu);
         c.fill = GridBagConstraints.BOTH;
         c.gridx = 0;
         c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 0;
+        pane.add(menuBar, c);
+
+        JSplitPane jSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        jSplitPane.setLeftComponent(leftPane);
+        jSplitPane.setRightComponent(rightPane);
+        jSplitPane.setDividerLocation(500);
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.gridy = 1;
         c.weightx = 1;
         c.weighty = 1;
         pane.add(jSplitPane, c);
@@ -53,7 +69,7 @@ public class Main extends JFrame implements Runnable{
         label.setHorizontalAlignment(JLabel.CENTER);
 
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 2;
         c.weightx = 1;
         c.weighty = 0;
         pane.add(label, c);
@@ -66,6 +82,17 @@ public class Main extends JFrame implements Runnable{
         this.setVisible(true);
     }
 
+    public void deselectTree(JTree tree, JTree selectedTree) {
+        MouseListener ml = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                tree.clearSelection();
+            }
+        };
+
+        selectedTree.addMouseListener(ml);
+    }
+
     public String getFullPath(TreePath selectedPath) {
         StringBuilder sb = new StringBuilder();
         Object[] nodes = selectedPath.getPath();
@@ -76,18 +103,16 @@ public class Main extends JFrame implements Runnable{
         return sb.toString();
     }
 
-
-    private JPanel createFileManagerTree() {
+    private JPanel createFileManagerTree(JTree fileManagerTree) {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout());
 
-        JTree fileManagerTree = new JTree(treeModel);
-
+        fileManagerTree.setModel(treeModel);
         fileManagerTree.setEditable(false);
         fileManagerTree.setShowsRootHandles(true);
 
         JTree finalFileManagerTree = fileManagerTree;
-        MouseListener ml = new MouseAdapter() {
+        /*MouseListener ml = new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 int selectedRow = finalFileManagerTree.getRowForLocation(e.getX(), e.getY());
@@ -127,7 +152,7 @@ public class Main extends JFrame implements Runnable{
         };
 
 
-        fileManagerTree.addMouseListener(ml);
+        fileManagerTree.addMouseListener(ml);*/
 
         panel.add(fileManagerTree);
 
